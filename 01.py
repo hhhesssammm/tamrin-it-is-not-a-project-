1f2877d62a534bcc73eb212b11d515cb0019a0b2
hhhesssammm <51443079+hhhesssammm@users.noreply.github.com>
2022-02-14 21:00:20 +0330

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,9 +1,12 @@
 import random
 numbers=[]
 max=0
+min=0
 for i in range(0,100):
     numbers.append(random.random())
     if numbers[i]>max:
         max=numbers[i]
+    if numbers[i]<min:
+        min=numbers[i]
 print(numbers)
 print(max)
